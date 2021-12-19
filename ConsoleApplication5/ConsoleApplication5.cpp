c0fa52d20a6c7bb7ae8e12d325be2b2cb98cdbe2
Denis <dlironov@bk.ru>
2021-12-20 02:05:58 +0700

    Добавьте файлы проекта.

---
+++ ConsoleApplication5/ConsoleApplication5.cpp
@@ -0,0 +1,17 @@
+﻿// ConsoleApplication5.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+#include <string>
+
+int main() {
+    setlocale(LC_ALL, "russian");
+    std::string str("Заменяем все вхождения ДЛЯ МЕНЯ строки на другую. Не хватает ДЛЯ МЕНЯ ложки.");
+    std::string findstr = "ДЛЯ МЕНЯ";
+    std::string replacestr = "одной";
+    std::string::size_type index;
+    while ((index = str.find(findstr)) != std::string::npos)
+        str.replace(index, findstr.size(), replacestr);
+    std::cout << str << std::endl;
+    return 0;
+}
